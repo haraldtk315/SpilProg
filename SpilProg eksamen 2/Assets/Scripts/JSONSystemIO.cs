5532f5c6b56a25661e27c2b4f0785cebc27b3188
haraldtk315 <haraldtk315@gmail.com>
2026-05-18 10:37:53 +0200

    JSON tweaks

--- SpilProg eksamen 2/Assets/Scripts/JSONSystemIO.cs
+++ SpilProg eksamen 2/Assets/Scripts/JSONSystemIO.cs
@@ -1,9 +1,14 @@
-using UnityEngine;
 using System.IO;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
 
 public class JSONSystemIO : MonoBehaviour
 {
     private string filePath;
+
+    public TMPro.TMP_Text scoreText;
+    private int score;
 
     void Start()
     {
@@ -21,19 +26,24 @@ public class JSONSystemIO : MonoBehaviour
         Debug.Log("Score gemt: " + score);
     }
 
-    public int LoadScore()
+    public void LoadScore()
     {
+        if (!File.Exists(filePath))
+        {
+            Debug.LogWarning("Save file does not exist: " + filePath);
+            scoreText.text = "0";
+        }
+
         if (File.Exists(filePath))
         {
             string json = File.ReadAllText(filePath);
             ScoreData data = JsonUtility.FromJson<ScoreData>(json);
+            score = data.score;
+            scoreText.text = score.ToString();
 
             Debug.Log("Score loaded: " + data.score);
-            return data.score;
-        }
 
-        Debug.LogWarning("Ingen save-fil fundet");
-        return 0;
+        } 
     }
 }
 
